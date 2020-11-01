e331f320525c552d6712d9a614859def15e251a2
IsmaelZerouk <73533997+IsmaelZerouk@users.noreply.github.com>
2020-11-01 18:25:23 +0100

    Delete movies.sql

--- movies.sql
+++
@@ -1,5 +0,0 @@
-
-UPDATE movies SET rank = ts_rank(lexemesSummary,plainto_tsquery((SELECT Summary FROM movies WHERE url='pirate-of-the-caribbean-the-curse-of-the-black-pearl')));
-CREATE TABLE recommendationsBasedOnSummaryField AS SELECT url, rank FROM movies WHERE rank > 0.01 ORDER BY rank DESC LIMIT 50;
-\copy (SELECT * FROM recommendationsBasedOnSummaryField) to '/home/pi/RSL/top50recommendations.csv' WITH csv;
-
